4481e7d3f0f0b8b3f58bc9651d46e2d504d79b70
Heyedos <hidosss@hotmail.com>
2026-04-09 17:58:35 +0300

    Add project files.

---
+++ ConsoleUI2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach (var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> 
+            {
+                new Product {ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product {ProductId=2,CategoryId=2,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+                new Product {ProductId=3,CategoryId=3,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+                new Product {ProductId=4,CategoryId=4,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product {ProductId=5,CategoryId=5,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            var entity = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+            if (entity is null) { Console.WriteLine("not Found"); return; }
+            _products.Remove(entity);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            var entity = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            if (entity is null) { Console.WriteLine("not Found"); return; }
+            entity.UnitPrice = product.UnitPrice;
+            entity.CategoryId = product.CategoryId;
+            entity.ProductName = product.ProductName;
+            entity.UnitsInStock = product.UnitsInStock;
+        }
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
