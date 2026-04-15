725dbfc8f775692997c0a7c19056996254092fb7
Heyedos <hidosss@hotmail.com>
2026-04-15 11:16:37 +0300

    adding ef core

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    // where T: a,b,c ile yapısı T generic constraint koyarız. -> T nin a,b,c ye uygun olmalı
+    // class -> referans olmalı , int double string olamaz
+    // IEntity -> IEntity interface'i veya IEntity interface'ini implamente eden bir nesne, class olabilir.
+    // new() -> instance'ı olan bir nesne olmalı -> abstract soyut nesneler olamaz -> abstract , interface nesneleri
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        List<T> GetAllByCategory(int entityId);
+    }
+}
